af4be6ebdaa1cab1ad8fac90b2500c255709ce99
Peter Luong <peter.luong678@gmail.com>
2024-06-09 16:53:39 +1000

    generate payment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_one :payment
+  accepts_nested_attributes_for :payment
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+
 end
