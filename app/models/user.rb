9031d06222d7a724d11d215333bf3c0312f9afa6
Peter Luong <peter.luong678@gmail.com>
2024-06-11 00:44:23 +1000

    registrations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :payment
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
 end
