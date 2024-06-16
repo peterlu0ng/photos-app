8f69ef1ac37a3da667867607a0818705d46cdae6
Peter Luong <peter.luong678@gmail.com>
2024-06-16 17:08:13 +1000

    add picture uploader and generate migration for images table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_043804) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_16_065430) do
+  create_table "images", force: :cascade do |t|
+    t.string "name_string"
+    t.string "picture"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
+
   create_table "pay_charges", force: :cascade do |t|
     t.bigint "customer_id", null: false
     t.bigint "subscription_id"
@@ -128,6 +137,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_043804) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "users"
   add_foreign_key "pay_charges", "pay_customers", column: "customer_id"
   add_foreign_key "pay_charges", "pay_subscriptions", column: "subscription_id"
   add_foreign_key "pay_payment_methods", "pay_customers", column: "customer_id"
