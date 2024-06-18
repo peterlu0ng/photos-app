e71f3aa17e2dc22866bdeef2f68bf45f59c7d3ed
Peter Luong <peter.luong678@gmail.com>
2024-06-18 12:03:57 +1000

    add paid column and only show upgrade if not paid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_16_065430) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_015218) do
   create_table "images", force: :cascade do |t|
     t.string "name_string"
     t.string "picture"
@@ -133,6 +133,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_16_065430) do
     t.datetime "updated_at", null: false
     t.string "city"
     t.string "country"
+    t.boolean "paid", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
